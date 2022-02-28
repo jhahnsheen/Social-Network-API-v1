daab5fa6afb0ef1f903b5616007a0728bb9892c8
jhahnsheen <91440103+jhahnsheen@users.noreply.github.com>
2022-02-28 04:37:48 -0500

    added models/index.js

---
+++ models/index.js
@@ -0,0 +1,7 @@
+// import User model
+const User = require('./User');
+// import Thought model
+const Thought = require('./Thought');
+
+// export the User and Thought models together
+module.exports = { User, Thought };
